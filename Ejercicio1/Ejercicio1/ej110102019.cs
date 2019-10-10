9c0bb3df9b098298ba7bbc26faa2357d2b8bca10
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-10 16:24:00 -0300

    ej1 10-10-2019

---
+++ Ejercicio1/Ejercicio1/ej110102019.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, suma, resta, producto;
+            
+            Console.Write("Ingresar primer número: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar segundo número: ");
+            b = int.Parse(Console.ReadLine());
+
+            suma = a + b;
+            resta = a - b;
+            producto = a * b;
+
+            Console.WriteLine("La suma de {0} + {1} es: {2}", a, b, suma);
+            Console.WriteLine("La suma de {0} - {1} es: {2}", a, b, resta);
+            Console.WriteLine("La suma de {0} * {1} es: {2}", a, b, producto);
+        }
+    }
+}
