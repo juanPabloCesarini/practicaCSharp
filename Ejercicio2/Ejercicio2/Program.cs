a2684058b44ccf493307ccf0e590b217545024b0
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-10 16:44:16 -0300

    Ejercicio2 10-10-2019

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double horas, precioPorHora, sueldo;
+            Console.Write("Ingrese cantidad de horas: ");
+            horas = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de la hora: ");
+            precioPorHora = double.Parse(Console.ReadLine());
+
+            sueldo = horas * precioPorHora;
+
+            Console.WriteLine("El sueldo a abonar es: $ {0}", sueldo);
+        }
+    }
+}
