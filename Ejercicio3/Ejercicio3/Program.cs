3783d80c10eb251810b3e98837c2f4e113a85996
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-10 22:46:54 -0300

    Ejercicio3 10-10-2019

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, promedio;
+            Console.Write("Nota uno: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota dos: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota tres: ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine("EL PROMEDIO ES: {0}", promedio);
+        }
+    }
+}
