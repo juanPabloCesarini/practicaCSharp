243b14004a9b975df5d66d5f261f2e4d6d3b937d
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-10 23:00:34 -0300

    Ejercicio4 10-10-2019

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ganados, empatados, totalG,totalE,puntos;
+            Console.Write("Cantidad de partidos ganados: ");
+            ganados = int.Parse(Console.ReadLine());
+            Console.Write("Cantidad de partidos empatados: ");
+            empatados = int.Parse(Console.ReadLine());
+           
+            totalG = ganados * 3;
+            totalE = empatados * 1;
+            puntos = totalG + totalE;
+
+            Console.WriteLine("El total del puntaje es: {0}", puntos);
+        }
+    }
+}
