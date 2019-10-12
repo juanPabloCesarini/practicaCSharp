272c77174feeba17a41b74f19b65099734e978ba
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-12 17:26:58 -0300

    ejercicio9 12-10-19

---
+++ clase1-04102019/Ejercicio9/Ejercicio9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido;
+            int edad;
+
+            Console.Write("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Apellido: ");
+            apellido = Console.ReadLine();
+            Console.Write("Edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("{0} {1}, es mayor", nombre, apellido);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}, es menor", nombre, apellido);
+            }
+            Console.ReadKey();
+        }
+    }
+}
