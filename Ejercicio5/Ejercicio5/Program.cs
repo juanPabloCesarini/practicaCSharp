dea40029a88367ea31db6f3342a111b293d6098a
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-12 09:33:29 -0300

    Hecho en la clase dlel 11-10-2019

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, total, promedio, p1, p2;
+            Console.Write("Ingresar valor A: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Ingresa calor B: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Ingresar varlor C: ");
+            c = double.Parse(Console.ReadLine());
+            Console.Write("Ingresar varlor D: ");
+            d = double.Parse(Console.ReadLine());
+
+            total = a + b + c + d;
+            promedio = total / 4;
+
+            p1 = (a * 100) / c;
+            p2 = (d * 100) / total;
+
+            Console.WriteLine("El promedio: {0}\nEl porcentaje 1: {1}\nEl porcentaje 2: {2}", promedio, p1, p2);
+        }
+    }
+}
---
+++ ej2enClase1110/ej2enClase1110/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ej2enClase1110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nroEmpleado, totalRegistros, empleadosA, empleadosB, empleadosC, empleadosD;
+            string cat;
+            totalRegistros = 0;
+            empleadosA = 0;
+            empleadosB = 0;
+            empleadosC = 0;
+            empleadosD = 0;
+
+            Console.Write("Ingrese nro de Empleado: ");
+            nroEmpleado = int.Parse(Console.ReadLine());
+
+            while (nroEmpleado != 0)
+            {
+                Console.Write("Ingrese la categoria: ");
+                cat = Console.ReadLine();
+                totalRegistros += 1;
+                switch (cat)
+                {
+                    case "a":
+                        empleadosA += 1;
+                        break;
+                    case "A":
+                        empleadosA += 1;
+                        break;
+                    case "b":
+                        empleadosB += 1;
+                        break;
+                    case "B":
+                        empleadosB += 1;
+                        break;
+                    case "c":
+                        empleadosC += 1;
+                        break;
+                    case "C":
+                        empleadosC += 1;
+                        break;
+                    case "d":
+                        empleadosD += 1;
+                        break;
+                    case "D":
+                        empleadosD += 1;
+                        break;
+                    default:
+                        Console.WriteLine("ERROR!!");
+                        break;
+
+                }
+                Console.Write("Ingrese nro de Empleado: ");
+                nroEmpleado = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Toital Ingredados {0}", totalRegistros);
+            Console.WriteLine("Total Empleados A: {0}\ntotal empleados B: {1}\ntotal empleados C: {2}\ntotal empleados D: {3}", empleadosA, empleadosB, empleadosC, empleadosD);
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
