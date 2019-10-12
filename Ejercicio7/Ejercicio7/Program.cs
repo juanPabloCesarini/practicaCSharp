39cbeb16839289a269fdd13b28ccd660a9d4acef
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-12 16:47:23 -0300

    ejercicio 7 12-10-2019

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Ingresar número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("ES NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("ES POSITIVO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
