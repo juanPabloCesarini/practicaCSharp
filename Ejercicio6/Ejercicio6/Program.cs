f26ba791695c8016676b45de803eb2dead5d5f51
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-12 16:38:54 -0300

    Ejercicio6 12-10-19

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hombres, mujeres, total;
+            double porcH, porcM;
+
+            Console.Write("Ingresar cantidad de hombres: ");
+            hombres = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingresar cantidad mujeres: ");
+            mujeres = int.Parse(Console.ReadLine());
+
+            total = hombres + mujeres;
+            porcH = (hombres * 100) / total;
+            porcM = (mujeres * 100) / total;
+
+            Console.Write("Total: {0}\nPprcentaje hombres: {1}\nProcentaje mujeres: {2}", total, porcH, porcM);
+            Console.ReadKey();
+        }
+    }
+}
