06f87ee553a2f75c56e363a92216426d5428e5c0
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-14 22:49:27 -0300

    ejercicios hechos en clase

---
+++ clase3-14102019/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar numero: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i =0; i<=15; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2} ", i, num, i * num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ clase3-14102019/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int atletas, cadetes, juveniles, veteranos, edad;
+            double tiempo, tiempoMin, tiempoMax, porcCateg, promPorCat, atletasAux;
+            string apellido;
+
+            Console.Write("ingrese cantidad de atletas: ");
+            atletas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= atletas; i++)
+            {
+                Console.WriteLine("Ingrese el apellido: ");
+                apellido = Console.ReadLine();
+                Console.WriteLine("ingrese edad: ");
+            }
+            Console.ReadKey();
+        }
+    }   
+}
