750db74ce508b603f02372ed88d2617033ab95ba
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-15 22:40:39 -0300

    Ejercicio 3 15-10-2019

--- clase3-14102019/Ejercicio3/Ejercicio3/Program.cs
+++ clase3-14102019/Ejercicio3/Ejercicio3/Program.cs
@@ -10,6 +10,41 @@ namespace Ejercicio3
     {
         static void Main(string[] args)
         {
+            int num, pos = 0, neg = 0, pares = 0, impares = 0;
+            string corte = "s";
+
+            
+
+            while (corte != "n")
+            {
+                Console.Write("Ingrease el número: ");
+                num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    pos += 1;
+                }
+                else
+                {
+                    neg += 1;
+                }
+                if (num % 2 == 0)
+                {
+                    pares += 1;
+                }
+                else
+                {
+                    impares += 1;
+                }
+                
+
+                Console.Write("Continua?? (s/n): ");
+                corte = Console.ReadLine();
+            }
+            Console.WriteLine("Positivos: {0}", pos);
+            Console.WriteLine("Negativos: {0}", neg);
+            Console.WriteLine("Pares: {0}", pares);
+            Console.WriteLine("Impares: {0}", impares);
+            Console.ReadKey();
         }
     }
 }
