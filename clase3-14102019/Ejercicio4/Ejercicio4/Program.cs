68fa9eed9d255be91c2fb163310ec45b148e71ab
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-15 23:09:30 -0300

    ejercicio4 15-10-2019

---
+++ clase3-14102019/Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0, acumulador = 0, numero;
+            double promedio = 0;
+
+            do
+            {
+                Console.Write("ingresa un número: ");
+                numero = int.Parse(Console.ReadLine());
+                contador += 1;
+                acumulador += numero;
+                promedio = acumulador / contador;
+            } while (numero != 0);
+            Console.Write("El promedio es: {0}", promedio);
+            Console.ReadKey();
+        }
+    }
+}
