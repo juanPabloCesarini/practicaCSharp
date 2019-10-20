7056b806b087b72116f3f254e72a00e196b7bc6a
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-20 13:22:59 -0300

    Ejercicio1 20-10-2019

---
+++ clase5-18102019/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        public static int sumarEdades(int[] vector)
+        {
+            int suma = 0;
+            for(int i=0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+            return suma;
+        }
+        public static double promediarEdades(int suma, int[] vector)
+        {
+            double prom = suma / vector.Length;
+            return prom; 
+        }
+        static void Main(string[] args)
+        {
+            int[] vector = new int[10];
+            int men = 0, may = 0, suma=0;
+
+            for (int i =0; i<vector.Length; i++)
+            {
+                Console.Write("Ingrese edad: ");
+                vector[i] = int.Parse(Console.ReadLine());
+                if (vector[i] < 18)
+                {
+                    men += 1;
+                }
+                else
+                {
+                    may += 1;
+                }
+
+            }
+            suma = sumarEdades(vector);
+            Console.WriteLine("La suma de edades es: {0}", suma);
+            Console.WriteLine("La edad promedio es: {0}", promediarEdades(suma, vector));
+            Console.WriteLine("Mayores de 18 años: {0}\nMenores de 18 años: {1}", may, men);
+            Console.ReadKey();
+        }
+    }
+}
